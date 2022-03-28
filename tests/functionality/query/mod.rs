4e68beb0f3ef1568906232c3af7a76a219928a83
Kyran Gostelow <kyran@gostelow.me>
2022-03-28 21:17:52 +1100

    `JOIN` Tests

--- tests/functionality/query/mod.rs
+++ tests/functionality/query/mod.rs
@@ -1,1 +1,1 @@
-crate::util_macros::make_all!(functionality::query, [with, aggregate, filter, function]);
+crate::util_macros::make_all!(functionality::query, [with, aggregate, filter, function, join]);
