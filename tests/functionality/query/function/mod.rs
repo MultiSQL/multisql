11c4292b5b1e608e769a03567e6a2b50951e1159
Kyran Gostelow <kyran@gostelow.me>
2022-03-28 20:53:29 +1100

    `CAST` Function Tests

---
+++ tests/functionality/query/function/mod.rs
@@ -0,0 +1,1 @@
+crate::util_macros::make_all!(functionality::query::function, [func_cast, misc_func_cast]);
--- tests/functionality/query/mod.rs
+++ tests/functionality/query/mod.rs
@@ -1,1 +1,1 @@
-crate::util_macros::make_all!(functionality::query, [with, aggregate, filter]);
+crate::util_macros::make_all!(functionality::query, [with, aggregate, filter, function]);
