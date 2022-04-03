d3d1bf48bf38477684273648e88d8b76065ccd8e
Kyran Gostelow <kyran@gostelow.me>
2022-04-03 12:21:37 +1000

    Reimplement old comparison test

--- tests/functionality/query/filter/mod.rs
+++ tests/functionality/query/filter/mod.rs
@@ -1,1 +1,1 @@
-crate::util_macros::make_all!(functionality::query::filter, [arithmetic, indexed, filter_simple]);
+crate::util_macros::make_all!(functionality::query::filter, [arithmetic, indexed, filter_simple, gt_lt, indexed_gt_lt]);
