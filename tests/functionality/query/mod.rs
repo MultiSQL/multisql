96bf6cf21fb5ac77655da7b06c8d980ce2c780bb
Kyran Gostelow <kyran@gostelow.me>
2022-04-03 14:05:06 +1000

    Reimplement and improve secondary aggregate test

--- tests/functionality/query/mod.rs
+++ tests/functionality/query/mod.rs
@@ -1,1 +1,1 @@
-crate::util_macros::make_all!(functionality::query, [with, aggregate, filter, function, join]);
+crate::util_macros::make_all!(functionality::query, [with, aggregate, aggregate_old, filter, function, join]);
