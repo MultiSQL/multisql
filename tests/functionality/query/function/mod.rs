7db244f025b8baee45a6f6f20b69d3439ea788b7
Kyran Gostelow <kyran@gostelow.me>
2022-04-03 14:20:39 +1000

    `DATEADD` test

--- tests/functionality/query/function/mod.rs
+++ tests/functionality/query/function/mod.rs
@@ -1,6 +1,6 @@
 crate::util_macros::make_all!(functionality::query::function, [
 	func_cast, misc_func_cast,
-	convert,
+	convert, convert_timestamp,
 	left_right, upper_lower,
 	case,
 	func_other, func_timestamp
