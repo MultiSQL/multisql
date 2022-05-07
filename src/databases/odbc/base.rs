d25bb400b3361333396518780324821ad04f4d10
Kyran Gostelow <kyran@gostelow.me>
2022-05-07 14:37:09 +1000

    Parse results

--- src/databases/odbc/base.rs
+++ src/databases/odbc/base.rs
@@ -1,7 +1,7 @@
 use {
 	crate::{DBBase, ODBCDatabase, Result, Schema},
 	async_trait::async_trait,
-	odbc_api::Cursor,
+	odbc_api::{Cursor, ResultSetMetadata},
 };
 
 #[async_trait(?Send)]
@@ -11,10 +11,21 @@ impl DBBase for ODBCDatabase {
 			.environment
 			.connect_with_connection_string(&self.connection_string)?;
 		let mut tables = connection.tables(&connection.current_catalog()?, "", "", "")?;
-		let mut output = Vec::new();
-		tables.next_row()?.unwrap().get_text(1, &mut output)?;
-		println!("{}", std::str::from_utf8(&output).unwrap());
-		Ok(Vec::new())
+		let col_range = (0..tables.num_result_cols()?);
+		let mut schemas = Vec::new();
+		while let Some(mut row) = tables.next_row()? {
+			let row = col_range
+				.clone()
+				.map(|col| {
+					let mut output = Vec::new();
+					row.get_text(col as u16, &mut output)?;
+					let output = std::str::from_utf8(&output)?.to_string();
+					Ok(output)
+				})
+				.collect::<Result<Vec<String>>>()?;
+			println!("{:?}", row);
+		}
+		Ok(schemas)
 	}
 	async fn fetch_schema(&self, _table_name: &str) -> Result<Option<Schema>> {
 		Ok(Some(self.scan_schemas().await?.remove(0)))
