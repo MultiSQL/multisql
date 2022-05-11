7e0be167f57b708b44dcd50bd10c25c301136d5f
Kyran Gostelow <kyran@gostelow.me>
2022-05-11 18:40:05 +1000

    a

--- src/databases/odbc/auto_increment.rs
+++ src/databases/odbc/auto_increment.rs
@@ -1,7 +1,28 @@
 use {
-	crate::{AutoIncrement, ODBCDatabase},
+	crate::{AutoIncrement, ODBCDatabase, Result},
 	async_trait::async_trait,
 };
 
 #[async_trait(?Send)]
-impl AutoIncrement for ODBCDatabase {}
+impl AutoIncrement for ODBCDatabase {
+	async fn generate_increment_values(
+		&mut self,
+		table_name: String,
+		columns: Vec<(usize, String, i64)>,
+	) -> Result<Vec<((usize, String), i64)>> {
+		let row_init = 1;
+		Ok(columns
+			.into_iter()
+			.map(|(index, name, _)| ((index, name), row_init as i64))
+			.collect())
+	}
+
+	async fn set_increment_value(
+		&mut self,
+		_table_name: &str,
+		_column_name: &str,
+		_end: i64,
+	) -> Result<()> {
+		Ok(())
+	}
+}
