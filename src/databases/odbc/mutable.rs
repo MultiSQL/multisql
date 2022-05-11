58215568b82182c03b88e5af030804fae16a2a19
Kyran Gostelow <kyran@gostelow.me>
2022-05-11 18:28:41 +1000

    Maybe working slowly

--- src/databases/odbc/mutable.rs
+++ src/databases/odbc/mutable.rs
@@ -1,8 +1,8 @@
 use {
 	super::base::convert_table_name,
-	crate::{Cast, DBMut, ODBCDatabase, Result, Row, Value},
+	crate::{Cast, DBBase, DBMut, ODBCDatabase, Result, Row},
 	async_trait::async_trait,
-	odbc_api::buffers::TextRowSet,
+	odbc_api::IntoParameter,
 };
 
 #[async_trait(?Send)]
@@ -12,48 +12,32 @@ impl DBMut for ODBCDatabase {
 			.environment
 			.connect_with_connection_string(&self.connection_string)?;
 		let table_name = convert_table_name(table_name);
-		let buffer = TextRowSet::for_cursor(
-			4096,
-			&connection
-				.execute(
-					&format!("SELECT TOP 1 * FROM {table}", table = table_name),
-					(),
-				)?
-				.unwrap(),
-			Some(4096),
-		)?;
-		let rows = rows
-			.into_iter()
-			.map(|row| {
-				row.0
-					.into_iter()
-					.map(|cell| {
-						if matches!(cell, Value::Null) {
-							None
-						} else {
-							let string: String = cell.cast().unwrap(); // TODO: Handle
-							Some(string.as_bytes())
-						}
-					})
-					.collect::<Vec<Option<&[u8]>>>()
-					.as_slice()
-			})
-			.collect::<Vec<&[Option<&[u8]>]>>()
-			.as_slice();
-		let buffer = create_buffer(buffer, rows);
-		connection.execute(
-			&format!("INSERT INTO {table} VALUES ?", table = table_name),
-			&buffer,
-		)?;
+		let schema = self.fetch_schema(&table_name).await?.unwrap();
+		let columns = schema
+			.column_defs
+			.iter()
+			.map(|col_def| col_def.name.as_str())
+			.collect::<Vec<&str>>()
+			.join(", ");
+		let placeholders = schema
+			.column_defs
+			.iter()
+			.map(|_| "?")
+			.collect::<Vec<&str>>()
+			.join(", ");
+		let query = format!(
+			"INSERT INTO {table} ({columns}) VALUES ({placeholders})",
+			table = table_name,
+			columns = columns
+		);
+		for row in rows {
+			let values = row
+				.0
+				.into_iter()
+				.map(|value| value.cast().map(|value: String| value.into_parameter()))
+				.collect::<Result<Vec<_>>>()?;
+			connection.execute(&query, &values[..]).unwrap();
+		}
 		Ok(())
 	}
-}
-
-fn create_buffer(mut buffer: TextRowSet, rows: &[&[Option<&[u8]>]]) -> TextRowSet {
-	// Massive mess. This interface gave me a lot of trouble. TODO: Clean/rewrite
-	rows.into_iter().for_each(|row| {
-		let row: Vec<Option<&[u8]>> = row.into_iter().map(|cell| *cell).collect();
-		buffer.append(row.into_iter());
-	});
-	buffer
 }
