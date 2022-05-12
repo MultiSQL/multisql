a9e1b926c9255e9ca8555caa434e6cf0da64db2d
Kyran Gostelow <kyran@gostelow.me>
2022-05-12 18:23:22 +1000

    0.2.20

--- src/databases/odbc/mutable.rs
+++ src/databases/odbc/mutable.rs
@@ -92,7 +92,7 @@ fn into_buffer(values: Vec<Value>, column_def: Column) -> AnyColumnBuffer {
 				.collect(),
 		),
 		Str => {
-			let mut col = TextColumn::new(255, values.len() * 1000);
+			let mut col = TextColumn::new(50, values.len());
 			values.into_iter().enumerate().for_each(|(index, value)| {
 				let text: String = value.cast().unwrap();
 				col.append(index, Some(text.as_bytes()))
