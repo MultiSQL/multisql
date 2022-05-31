85dcc45c242525ec67ca4049ae985bac0a637025
Kyran Gostelow <kyran@gostelow.me>
2022-05-31 22:00:57 +1000

    0.3.9 Convert other into string for serde

--- src/data/value/serde_convert.rs
+++ src/data/value/serde_convert.rs
@@ -1,14 +1,18 @@
-use crate::Value;
+use crate::{Cast, Value};
 
 impl From<Value> for serde_json::value::Value {
 	fn from(value: Value) -> serde_json::value::Value {
 		match value {
 			Value::Bool(value) => value.into(),
+			Value::U64(value) => value.into(),
 			Value::I64(value) => value.into(),
 			Value::F64(value) => value.into(),
 			Value::Str(value) => value.into(),
 			Value::Null => serde_json::value::Value::Null,
-			_ => unimplemented!(),
+			other => {
+				let string: String = other.cast().unwrap();
+				string.into()
+			}
 		}
 	}
 }
